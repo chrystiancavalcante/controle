1c17ab4e5118b9b2452a69353c5f0364fed12944
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-02-20 20:12:25 -0300

    first commit

--- src/container/estoque.js
+++ src/container/estoque.js
@@ -13,36 +13,36 @@ export default class estoque extends Component {
                 <form>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                            <label for="inputNome4">Nome</label>
-                            <input type="name" className="form-control" id="inputNome4" placeholder="Nome" />
+                            <label for="inputNome4">Nome do Produto</label>
+                            <input type="name" className="form-control" id="inputNome4" placeholder="Nome do Produto" />
                         </div>
                         <div className="form-group col-md-6">
-                            <label for="inputTelefone4">Telefone</label>
-                            <input type="Telefone" className="form-control" id="inputTelefone4" placeholder="Telefone" />
+                            <label for="inputTelefone4">Categoria</label>
+                            <input type="Telefone" className="form-control" id="inputTelefone4" placeholder="Categoria" />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label for="inputAddress">Endereço</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="Av. 56" />
+                        <label for="inputAddress">Fornecedor</label>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="Nome" />
                     </div>
                     <div className="form-group">
                         <label for="inputAddress2">Complemento</label>
-                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartamento, studio, etc." />
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Endereço." />
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                            <label for="inputCity">Cidade</label>
+                            <label for="inputCity">Quantidade</label>
                             <input type="text" className="form-control" id="inputCity" />
                         </div>
                         <div className="form-group col-md-4">
-                            <label for="inputState">Estado</label>
+                            <label for="inputState">Entrada</label>
                             <select id="inputState" className="form-control">
-                                <option selected>Estado...</option>
+                                <option selected>Entrada...</option>
                                 <option>...</option>
                             </select>
                         </div>
                         <div className="form-group col-md-2">
-                            <label for="inputZip">Cep</label>
+                            <label for="inputZip">Tipo</label>
                             <input type="text" className="form-control" id="inputZip" />
                         </div>
                     </div>
